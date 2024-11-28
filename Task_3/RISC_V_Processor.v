e0371272336da76a32ed10e65b5d07df701756ea
zuhair <122153573+syedzuhairabbasrizvi@users.noreply.github.com>
2024-11-28 14:26:33 +0500

    Update RISC_V_Processor.v

--- Task_3/RISC_V_Processor.v
+++ Task_3/RISC_V_Processor.v
@@ -8,6 +8,8 @@ module RISC_V_Processor(
     output reg [63:0] Write_Data_for_Reg, ReadData1, ReadData2, Imm_Data, 
     output reg [63:0] b, Result, 
     output reg [63:0] Read_Data_from_Mem,
+    output reg stall,
+    output reg flush,
     output reg [6:0] opcode,
     output reg IDEX_MemRead, EM_MemRead,
     output reg Branch, EM_Branch, MemRead, MemWrite, MemtoReg, RegWrite, 
@@ -19,7 +21,8 @@ module RISC_V_Processor(
     MW_Read_Data_from_Mem,
     output reg final_branch,
     output reg [3:0] IDEX_Funct,
-    output reg EM_addermuxselect
+    output reg EM_addermuxselect,
+    output reg [1:0] ForwardA, Forwardb
     
     );
 //to do: inspect mux_selector + addermuxselect
@@ -44,6 +47,8 @@ wire [63:0] Out_2;
 wire Mux_Selector;
 wire BLT;
 wire [63:0] b;
+wire stall;
+wire flush;
 
 wire [63:0] IFID_PC_Out;
 wire [31:0] Instruction;
@@ -53,7 +58,7 @@ wire IDEX_Branch, IDEX_MemRead, IDEX_MemWrite, IDEX_MemtoReg, IDEX_RegWrite, IDE
 EM_Branch, EM_MemRead, EM_MemWrite, EM_MemtoReg, EM_RegWrite, EM_Zero,
 MW_MemtoReg, MW_RegWrite, PC_Write, IFID_Write, IDEX_MuxOut, EM_addermuxselect;
 
-wire [1:0] IDEX_ALUOp;
+wire [1:0] IDEX_ALUOp, ForwardA, ForwardB;
 wire [3:0] IDEX_Funct;
 wire [4:0] IDEX_rs1, IDEX_rs2, IDEX_rd, EM_RD, MW_RD;
 
@@ -62,7 +67,11 @@ wire [63:0] IDEX_PC_Out, IDEX_ReadData1, IDEX_ReadData2, IDEX_Imm,
             mux1out, mux2out, mux3out;
 
 wire final_branch;
+FlushPipeline FP(final_branch, flush);
 
+Hazard_Detection_Unit HDU(rs1, rs2, IDEX_rd, IDEX_MemRead, stall); // .IFID_Write(IFID_Write), .PC_Write(PC_Write), .IDEX_MuxOut(IDEX_MuxOut), 
+
+wire [63:0] F1_1, F2_1;
 wire [4:0] rs1, rs2, rd;
 wire [63:0] ReadData1, ReadData2, Result;
 wire [63:0] Read_Data_from_Mem;
@@ -73,7 +82,7 @@ Adder add_1(PC_Out, 64'd4, Out_1);
 mux_2_1 mux2(final_branch, Out_1, EM_Adder2Out, PC_In);
 Program_Counter PC(clk, reset, stall, PC_In, PC_Out);
 Instruction_Memory IM(PC_Out, Instruction);
-IFID ifid(clk, reset, Instruction, PC_Out, IFID_Instruction, IFID_PC_Out);
+IFID ifid(clk, reset, stall, flush, Instruction, PC_Out, IFID_Instruction, IFID_PC_Out);
 
 instruction_parser IP(IFID_Instruction, opcode, rd, funct3, rs1, rs2, funct7);
 imm_gen IDE(IFID_Instruction, Imm_Data);
@@ -81,7 +90,7 @@ Control_Unit CU(opcode, stall, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Br
 registerFile Regs(Write_Data_for_Reg, rs1, rs2, MW_RD, MW_RegWrite, clk, reset, ReadData1, ReadData2);
 assign Funct = {IFID_Instruction[30], funct3};           //add complement bit to funct for ALU control
 
-IDEX idex(clk, reset, Branch, MemRead, MemWrite, MemtoReg, RegWrite, ALUSrc, ALUOp,
+IDEX idex(clk, reset, flush, Branch, MemRead, MemWrite, MemtoReg, RegWrite, ALUSrc, ALUOp,
         Funct, rs1, rs2, rd, IFID_PC_Out, ReadData1, ReadData2, Imm_Data,
         IDEX_Branch, IDEX_MemRead, IDEX_MemWrite, IDEX_MemtoReg, IDEX_RegWrite, IDEX_ALUSrc,
         IDEX_ALUOp, IDEX_Funct, IDEX_rs1, IDEX_rs2, IDEX_rd,
@@ -90,10 +99,12 @@ IDEX idex(clk, reset, Branch, MemRead, MemWrite, MemtoReg, RegWrite, ALUSrc, ALU
 Adder add_2(IDEX_PC_Out, IDEX_Imm*2, Out_2);
 ALU_Control AC(IDEX_ALUOp, IDEX_Funct, Operation);
 
-mux_2_1 f3(IDEX_ALUSrc, IDEX_ReadData2, IDEX_Imm, b);
-ALU alu(IDEX_ReadData1, b, Operation, Result, Zero, BLT);
+mux_3_1 f1(IDEX_ReadData1, Write_Data_for_Reg, EM_Result, ForwardA, F1_1);
+mux_3_1 f2(IDEX_ReadData2, Write_Data_for_Reg, EM_Result, ForwardB, F2_1);
+mux_2_1 f3(IDEX_ALUSrc, F2_1, IDEX_Imm, b);
+ALU alu(F1_1, b, Operation, Result, Zero, BLT);
 
-EXMEM EM(clk, reset, IDEX_Branch, IDEX_MemRead, IDEX_MemWrite, IDEX_MemtoReg, IDEX_RegWrite,
+EXMEM EM(clk, reset, flush, IDEX_Branch, IDEX_MemRead, IDEX_MemWrite, IDEX_MemtoReg, IDEX_RegWrite,
         Zero, Mux_Selector, IDEX_rd, Out_2, Result, F2_1, 
         EM_Branch, EM_MemRead, EM_MemWrite, EM_MemtoReg, EM_RegWrite, EM_Zero, EM_addermuxselect, EM_RD, EM_Adder2Out, EM_Result, EM_WriteData);
 assign final_branch = EM_Branch && EM_addermuxselect;
@@ -102,6 +113,8 @@ MEMWB MW(clk, reset, EM_MemtoReg, EM_RegWrite, EM_RD, EM_Result, Read_Data_from_
         MW_MemtoReg, MW_RegWrite, MW_RD, MW_Result, MW_Read_Data_from_Mem);
 
 mux_2_1 mux3(MW_MemtoReg, MW_Result, MW_Read_Data_from_Mem, Write_Data_for_Reg);
+
+ForwardingUnit FU(IDEX_rs1, IDEX_rs2, EM_RD, MW_RD, MW_RegWrite, EM_RegWrite, ForwardA, ForwardB);
 
 Branch_Unit BU(IDEX_Funct,IDEX_ReadData1,b,Mux_Selector);
 
